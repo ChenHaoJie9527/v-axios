2aea10c22f13518c4fc65cfea03b96357a2e3233
chenhaojie <a18520145493@163.com>
2022-10-27 18:55:36 +0800

    fix: 修复tool类型

--- src/api/tool/index.ts
+++ src/api/tool/index.ts
@@ -1,68 +1,42 @@
+import type { AuthErrorMap, NetworkErrorState } from "./type";
+import { ElMessage } from "element-plus";
 // 业务处理函数
-
 export const handleRequestHeader = (config: Record<string, any>) => {
   config["xxx"] = "xxx";
   return config;
 };
 
 export const handleAuth = (config: Record<string, any>) => {
-  config.header["token"] = localStorage.getItem("token") || "";
+  config.headers["token"] = localStorage.getItem("token") || "";
   return config;
 };
 
-export const handleNetworkError = (errorState: number) => {
-  let errMessage = "网络繁忙";
+export const handleNetworkError = (errorState: NetworkErrorState) => {
+  const networkErrMap = {
+    "400": "错误的请求", // token 失效
+    "401": "未授权，请重新登录",
+    "403": "拒绝访问",
+    "404": "请求错误，未找到该资源",
+    "405": "请求方法未允许",
+    "408": "请求超时",
+    "500": "服务器端出错",
+    "501": "网络未实现",
+    "502": "网络错误",
+    "503": "服务不可用",
+    "504": "网络超时",
+    "505": "http版本不支持该请求",
+  };
   if (errorState) {
-    switch (errorState) {
-      case 400:
-        errMessage = "错误的请求";
-        break;
-      case 401:
-        errMessage = "未授权，请重新登录";
-        break;
-      case 403:
-        errMessage = "拒绝访问";
-        break;
-      case 404:
-        errMessage = "请求错误,未找到该资源";
-        break;
-      case 405:
-        errMessage = "请求方法未允许";
-        break;
-      case 408:
-        errMessage = "请求超时";
-        break;
-      case 500:
-        errMessage = "服务器端出错";
-        break;
-      case 501:
-        errMessage = "网络未实现";
-        break;
-      case 502:
-        errMessage = "网络错误";
-        break;
-      case 503:
-        errMessage = "服务不可用";
-        break;
-      case 504:
-        errMessage = "网络超时";
-        break;
-      case 505:
-        errMessage = "http版本不支持该请求";
-        break;
-      default:
-        errMessage = `其他连接错误 --${errorState}`;
-    }
-  } else {
-    errMessage = `无法连接到服务器！`;
+    ElMessage.error(
+      networkErrMap[errorState] ?? `其他连接错误 --${errorState}`
+    );
+    return;
   }
-
-  //   message.error(errMessage);
-  return errMessage;
+  ElMessage.error("网络繁忙");
 };
 
-export const handleAuthError = (error: string) => {
-  const authErrMap: Object = {
+export const handleAuthError = (error: AuthErrorMap) => {
+  const authErrMap = {
     "10031": "登录失效，需要重新登录", // token 失效
     "10032": "您太久没登录，请重新登录~", // token 过期
     "10033": "账户未绑定角色，请联系管理员绑定角色",
@@ -73,7 +47,7 @@ export const handleAuthError = (error: string) => {
     "10038": "账号未找到",
   };
   if (authErrMap.hasOwnProperty(error)) {
-    // message.error(authErrMap[errno]);
+    ElMessage.error(authErrMap[error]);
     // 授权错误，登出账户
     // logout();
     return false;
@@ -84,6 +58,7 @@ export const handleAuthError = (error: string) => {
 export const handleGeneralError = (errno: string, errmsg: string) => {
   if (errno !== "0") {
     // meessage.error(err.errmsg);
+    ElMessage.error(errmsg);
     return false;
   }
 
